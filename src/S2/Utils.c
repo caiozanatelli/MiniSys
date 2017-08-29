7dbe8cb0a72ed6d0e5d298c47260c7114ed2e30d
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2017-08-29 18:17:58 -0300

    Moving macros for the header file

--- src/S2/Utils.c
+++ src/S2/Utils.c
@@ -1,25 +1,6 @@
 #include <string.h>
 #include "Utils.h"
 
-// Macros for identifying instructions
-#define LAD 1
-#define SAD 2
-#define ADD 3
-#define SUB 4
-#define INP 5
-#define OUT 6
-#define JMP 7
-#define JGZ 8
-#define JLZ 9
-#define JZE 10
-#define HLT 11
-#define LXD 12
-#define SXD 13
-#define LAX 14
-#define SAX 15
-#define LCD 16
-#define JCC 17
-
 int isLabel(char *s) {
 	return (strlen(s) == 1);
 }
