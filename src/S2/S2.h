2f5851e8898f9154bed20756357bd1a4891d9daa
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2017-08-29 18:18:38 -0300

    Adding instructions macros

--- src/S2/S2.h
+++ src/S2/S2.h
@@ -3,6 +3,25 @@
 
 #include "SymbolTable.h"
 
+// Macros for identifying instructions
+#define LAD 1
+#define SAD 2
+#define ADD 3
+#define SUB 4
+#define INP 5
+#define OUT 6
+#define JMP 7
+#define JGZ 8
+#define JLZ 9
+#define JZE 10
+#define HLT 11
+#define LXD 12
+#define SXD 13
+#define LAX 14
+#define SAX 15
+#define LCD 16
+#define JCC 17
+
 void genM2Code(FILE **inputFile, FILE **outputFile);
 void parseInstructions(SymbolTable *table, FILE **inputFile, FILE **outputFile);
 void writeInstruction(SymbolTable *table, FILE **outputFile, char *reader, char *instruction, int ILC);
